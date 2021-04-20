63a35b76f46ef65a56d8aafcd9d7f39f8c0022ca
Da <da@tubi.tv>
2021-04-20 15:55:26 +0800

    Update pretrained model

--- resnet.py
+++ resnet.py
@@ -5,8 +5,10 @@ import subprocess
 model_path = "/tmp/model/resnet50.pt"
 
 # Download pre-trained resnet model
+# Here is where I found the pretrained model:
+# https://github.com/pytorch/vision/blob/8fb5838ca916fd4ace080dae0357e7c307037bef/torchvision/models/detection/faster_rcnn.py#L291
 if not os.path.exists(model_path):
-    subprocess.check_call(f"wget https://download.pytorch.org/models/resnet50-19c8e357.pth -O {model_path}", shell=True)
+    subprocess.check_call(f"wget https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth -O {model_path}", shell=True)
 else:
     print("Model already downloaded...")
 
@@ -19,6 +21,9 @@ create model resnet_m
 USING '{work_dir}/model/resnet_spec.yaml'
 """)
 
-spark.sql(f"""
+result = spark.sql(f"""
 select ML_PREDICT(resnet_m, '{work_dir}/img/lena.png')
-""").show()
+""")
+
+result.printSchema()
+result.show()
