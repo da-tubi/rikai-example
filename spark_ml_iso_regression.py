f451785a9bb0abdcfa0203f10e74bc5ec74a3a43
Renkai Ge <renkaige@tubi.tv>
2021-10-18 15:26:34 +0800

    debugging

--- spark_ml_iso_regression.py
+++ spark_ml_iso_regression.py
@@ -54,11 +54,35 @@ with mlflow.start_run() as run:
     df.createOrReplaceTempView("tbl_X")
 
     # TODO
-    # pyspark.sql.utils.AnalysisException: cannot resolve 'array(tbl_x.`label`, tbl_x.`features`)' due to data type mismatch:
-    # input to function array should all be the same type,
-    # but it's [double, struct<type:tinyint,size:int,indices:array<int>,values:array<double>>]; line 2 pos 40;
+    #  TypeError: cannot pickle 'generator' object
+    # 	at org.apache.spark.api.python.BasePythonRunner$ReaderIterator.handlePythonException(PythonRunner.scala:517)
+    # 	at org.apache.spark.sql.execution.python.PythonUDFRunner$$anon$2.read(PythonUDFRunner.scala:84)
+    # 	at org.apache.spark.sql.execution.python.PythonUDFRunner$$anon$2.read(PythonUDFRunner.scala:67)
+    # 	at org.apache.spark.api.python.BasePythonRunner$ReaderIterator.hasNext(PythonRunner.scala:470)
+    # 	at org.apache.spark.InterruptibleIterator.hasNext(InterruptibleIterator.scala:37)
+    # 	at scala.collection.Iterator$$anon$11.hasNext(Iterator.scala:489)
+    # 	at scala.collection.Iterator$$anon$10.hasNext(Iterator.scala:458)
+    # 	at scala.collection.Iterator$$anon$10.hasNext(Iterator.scala:458)
+    # 	at org.apache.spark.sql.catalyst.expressions.GeneratedClass$GeneratedIteratorForCodegenStage1.processNext(Unknown Source)
+    # 	at org.apache.spark.sql.execution.BufferedRowIterator.hasNext(BufferedRowIterator.java:43)
+    # 	at org.apache.spark.sql.execution.WholeStageCodegenExec$$anon$1.hasNext(WholeStageCodegenExec.scala:755)
+    # 	at org.apache.spark.sql.execution.SparkPlan.$anonfun$getByteArrayRdd$1(SparkPlan.scala:345)
+    # 	at org.apache.spark.rdd.RDD.$anonfun$mapPartitionsInternal$2(RDD.scala:898)
+    # 	at org.apache.spark.rdd.RDD.$anonfun$mapPartitionsInternal$2$adapted(RDD.scala:898)
+    # 	at org.apache.spark.rdd.MapPartitionsRDD.compute(MapPartitionsRDD.scala:52)
+    # 	at org.apache.spark.rdd.RDD.computeOrReadCheckpoint(RDD.scala:373)
+    # 	at org.apache.spark.rdd.RDD.iterator(RDD.scala:337)
+    # 	at org.apache.spark.scheduler.ResultTask.runTask(ResultTask.scala:90)
+    # 	at org.apache.spark.scheduler.Task.run(Task.scala:131)
+    # 	at org.apache.spark.executor.Executor$TaskRunner.$anonfun$run$3(Executor.scala:497)
+    # 	at org.apache.spark.util.Utils$.tryWithSafeFinally(Utils.scala:1439)
+    # 	at org.apache.spark.executor.Executor$TaskRunner.run(Executor.scala:500)
+    # 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+    # 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+    # 	at java.lang.Thread.run(Thread.java:748)
+
     result = spark.sql(f"""
-    select ML_PREDICT(mlflow_sklearn_m, array(label, features)) from tbl_X
+    select ML_PREDICT(mlflow_sklearn_m, struct(label, features)) from tbl_X
     """)
 
     result.printSchema()
