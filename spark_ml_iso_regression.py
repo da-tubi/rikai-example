09d6bb5acd6190e7c1777cd0b3b941a72f5c85e7
Renkai Ge <renkaige@tubi.tv>
2021-10-19 10:20:52 +0800

    debugging

--- spark_ml_iso_regression.py
+++ spark_ml_iso_regression.py
@@ -53,36 +53,35 @@ with mlflow.start_run() as run:
     df = dataset.toDF("label", "features")
     df.createOrReplaceTempView("tbl_X")
 
-    # TODO
-    #  TypeError: cannot pickle 'generator' object
-    # 	at org.apache.spark.api.python.BasePythonRunner$ReaderIterator.handlePythonException(PythonRunner.scala:517)
-    # 	at org.apache.spark.sql.execution.python.PythonUDFRunner$$anon$2.read(PythonUDFRunner.scala:84)
-    # 	at org.apache.spark.sql.execution.python.PythonUDFRunner$$anon$2.read(PythonUDFRunner.scala:67)
-    # 	at org.apache.spark.api.python.BasePythonRunner$ReaderIterator.hasNext(PythonRunner.scala:470)
-    # 	at org.apache.spark.InterruptibleIterator.hasNext(InterruptibleIterator.scala:37)
-    # 	at scala.collection.Iterator$$anon$11.hasNext(Iterator.scala:489)
-    # 	at scala.collection.Iterator$$anon$10.hasNext(Iterator.scala:458)
-    # 	at scala.collection.Iterator$$anon$10.hasNext(Iterator.scala:458)
-    # 	at org.apache.spark.sql.catalyst.expressions.GeneratedClass$GeneratedIteratorForCodegenStage1.processNext(Unknown Source)
-    # 	at org.apache.spark.sql.execution.BufferedRowIterator.hasNext(BufferedRowIterator.java:43)
-    # 	at org.apache.spark.sql.execution.WholeStageCodegenExec$$anon$1.hasNext(WholeStageCodegenExec.scala:755)
-    # 	at org.apache.spark.sql.execution.SparkPlan.$anonfun$getByteArrayRdd$1(SparkPlan.scala:345)
-    # 	at org.apache.spark.rdd.RDD.$anonfun$mapPartitionsInternal$2(RDD.scala:898)
-    # 	at org.apache.spark.rdd.RDD.$anonfun$mapPartitionsInternal$2$adapted(RDD.scala:898)
-    # 	at org.apache.spark.rdd.MapPartitionsRDD.compute(MapPartitionsRDD.scala:52)
-    # 	at org.apache.spark.rdd.RDD.computeOrReadCheckpoint(RDD.scala:373)
-    # 	at org.apache.spark.rdd.RDD.iterator(RDD.scala:337)
-    # 	at org.apache.spark.scheduler.ResultTask.runTask(ResultTask.scala:90)
-    # 	at org.apache.spark.scheduler.Task.run(Task.scala:131)
-    # 	at org.apache.spark.executor.Executor$TaskRunner.$anonfun$run$3(Executor.scala:497)
-    # 	at org.apache.spark.util.Utils$.tryWithSafeFinally(Utils.scala:1439)
-    # 	at org.apache.spark.executor.Executor$TaskRunner.run(Executor.scala:500)
-    # 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
-    # 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
-    # 	at java.lang.Thread.run(Thread.java:748)
+    df.show()
 
+    # : org.apache.spark.SparkException: Job aborted due to stage failure: Task 0 in stage 8.0 failed 1 times, most recent failure: Lost task 0.0 in stage 8.0 (TID 8) (192.168.1.241 executor driver): java.lang.UnsupportedOperationException: Unsupported data type: struct<type:tinyint,size:int,indices:array<int>,values:array<double>>
+    # 	at org.apache.spark.sql.util.ArrowUtils$.toArrowType(ArrowUtils.scala:57)
+
+    #   def toArrowType(dt: DataType, timeZoneId: String): ArrowType = dt match {
+    #     case BooleanType => ArrowType.Bool.INSTANCE
+    #     case ByteType => new ArrowType.Int(8, true)
+    #     case ShortType => new ArrowType.Int(8 * 2, true)
+    #     case IntegerType => new ArrowType.Int(8 * 4, true)
+    #     case LongType => new ArrowType.Int(8 * 8, true)
+    #     case FloatType => new ArrowType.FloatingPoint(FloatingPointPrecision.SINGLE)
+    #     case DoubleType => new ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)
+    #     case StringType => ArrowType.Utf8.INSTANCE
+    #     case BinaryType => ArrowType.Binary.INSTANCE
+    #     case DecimalType.Fixed(precision, scale) => new ArrowType.Decimal(precision, scale)
+    #     case DateType => new ArrowType.Date(DateUnit.DAY)
+    #     case TimestampType =>
+    #       if (timeZoneId == null) {
+    #         throw new UnsupportedOperationException(
+    #           s"${TimestampType.catalogString} must supply timeZoneId parameter")
+    #       } else {
+    #         new ArrowType.Timestamp(TimeUnit.MICROSECOND, timeZoneId)
+    #       }
+    #     case _ =>
+    #       throw new UnsupportedOperationException(s"Unsupported data type: ${dt.catalogString}")
+    #   }
     result = spark.sql(f"""
-    select ML_PREDICT(mlflow_sklearn_m, struct(label, features)) from tbl_X
+    select ML_PREDICT(mlflow_sklearn_m, named_struct('label',label,'features' ,features)) from tbl_X
     """)
 
     result.printSchema()
