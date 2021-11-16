0692f4e8cb3627ce507ac0a1db2f7c880fc64156
Darcy Shen <66561265+da-tubi@users.noreply.github.com>
2021-11-16 14:45:55 +0800

    spark-video on elephants dream (#5)

---
+++ elephants_dream.py
@@ -0,0 +1,27 @@
+from example import spark
+
+df = spark.read.format("video").option("fps", 5).load("elephants_dream.mp4")
+(
+df
+    .repartition(4)
+    .write
+    .format("rikai")
+    .mode("overwrite")
+    .save("/tmp/rikai_example/elephants_dream")
+)
+
+(
+spark
+    .read
+    .format("parquet")
+    .load("/tmp/rikai_example/elephants_dream")
+    .createOrReplaceTempView("elephants_dream")
+)
+
+spark.sql("select count(*) from elephants_dream").show()
+
+(
+spark
+    .sql("select frame_id, date_format(ts, 'mm:ss') from elephants_dream where frame_id < 100 order by frame_id asc limit 15")
+    .show()
+)
