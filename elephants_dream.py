38addcb91d6a5abc82be307f2040a8d598c00a9a
Darcy Shen <da@tubi.tv>
2021-11-16 14:31:24 +0800

    elephants_dream

--- elephants_dream.py
+++ elephants_dream.py
@@ -10,3 +10,17 @@ df
     .mode("overwrite")
     .save("/tmp/rikai_example/elephants_dream")
 )
+
+(
+spark
+    .read
+    .format("parquet")
+    .load("/tmp/rikai_example/elephants_dream")
+    .createOrReplaceTempView("elephants_dream")
+)
+
+(
+spark
+    .sql("select frame_id, date_format(ts, 'mm:ss') from elephants_dream where frame_id < 100 order by frame_id asc limit 10")
+    .show()
+)
