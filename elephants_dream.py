bfe29c300def594a41f321d21b3833deda7d1e9e
Darcy Shen <da@tubi.tv>
2021-11-16 14:43:53 +0800

    elephants_dream

--- elephants_dream.py
+++ elephants_dream.py
@@ -1,12 +1,11 @@
 from example import spark
 
-df = spark.read.format("video").load("elephants_dream.mp4")
+df = spark.read.format("video").option("fps", 5).load("elephants_dream.mp4")
 (
 df
     .repartition(4)
     .write
     .format("rikai")
-    .option("fps", 5)
     .mode("overwrite")
     .save("/tmp/rikai_example/elephants_dream")
 )
@@ -19,8 +18,10 @@ spark
     .createOrReplaceTempView("elephants_dream")
 )
 
+spark.sql("select count(*) from elephants_dream").show()
+
 (
 spark
-    .sql("select frame_id, date_format(ts, 'mm:ss') from elephants_dream where frame_id < 100 order by frame_id asc limit 10")
+    .sql("select frame_id, date_format(ts, 'mm:ss') from elephants_dream where frame_id < 100 order by frame_id asc limit 15")
     .show()
 )
